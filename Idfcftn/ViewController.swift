11626aa979b1406d281e7300c7368b0cd91b433a
The Dino king <jayjayasinghe9@gmail.com>
2019-09-02 08:16:03 +0630

    INITAL COMMIT WITH README

---
+++ Idfcftn/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Idfcftn
+//
+//  Created by user158640 on 9/2/19.
+//  Copyright © 2019 user158640. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("dkosf");
+    }
+
+
+}
+
